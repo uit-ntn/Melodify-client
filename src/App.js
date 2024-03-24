9e3b67292d8b4a036c4952358507fec4d8fa5ee6
Thanh Nhan <npthanhnhan2003@gmail.com>
2024-03-24 23:54:32 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import "./App.css";
 import { BrowserRouter as Router, Route, Routes, BrowserRouter } from "react-router-dom";
+import { useContext } from "react";
+import { LoggedContext } from "./Components/Login-SignUp/LoggedContext";
 import { publicRoutes, privateRoutes, adminRoutes } from "./routes/routes";
+import SideBar from "./Components/SideBar/SideBar";
+import SideBarForAdmin from "./Components/SideBar/SideBarForAdmin";
+import SideBarForDistributor from "./Components/SideBar/SideBarForDistributor";
+import "./App.css";
 import "tailwindcss/tailwind.css";
 
 
 function App() {
+
+  const { logged } = useContext(LoggedContext)
+
+
   // dành cho người dùng chưa signin
   const userID = 0;
   const accType = 0;
@@ -18,42 +27,70 @@ function App() {
     accType = userJson.accountTypeID;
   }
   return (
-      <Router>
-        <Routes>
-          {publicRoutes.map((route, index) => {
-            const Element = route.element;
-            return (
-              <Route
-                key={index}
-                path={route.path}
-                element={<Element />}
-              />
-            );
-          })}
+    <Router>
+      <div className="relative flex">
+        {logged ?
+          (accType === 3 ? <SideBarForAdmin /> :
+            accType === 4 ? <SideBarForDistributor /> : null)
+          :
+          <SideBar />}
+        <div className="flex-1 flex flex-col bg-black">
+          <div className="flex-1 pb-40">
+            {
+              (userID == 0) ?
+                <Routes>
+                  {publicRoutes.map((route, index) => {
+                    const Element = route.element;
+                    return (
+                      <Route
+                        key={index}
+                        path={route.path}
+                        element={<Element />}
+                      />
+                    );
+                  })}
+                </Routes>
+                :
+                (accType == 3) ?
+                  <Routes>
+                    {privateRoutes.map((route, index) => {
+                      const Element = route.element;
+                      return (
+                        <Route
+                          key={index}
+                          path={route.path}
+                          element={<Element />}
+                        />
+                      );
+                    })}
+                  </Routes>
+                  :
+                  (accType == 4) ?
+                    <Routes>
+                      {adminRoutes.map((route, index) => {
+                        const Element = route.element;
+                        return (
+                          <Route
+                            key={index}
+                            path={route.path}
+                            element={<Element />}
+                          />
+                        );
+                      })}
+                    </Routes>
+                    :
+                    <Routes>
+                    </Routes>
+            }
+          </div>
+        </div>
+        {/* <div className="fixed bottom-0 w-full">
+        <Play setPlayingID={setPlayingID} setPlayingTrack={setPlayingTrack} playingData={playingData} play={play} isPlaying={isPlaying} setPlayingData={setPlayingData} playingTrack={playingTrack} trackInAlbum={trackInAlbum} setIsPlaying={setIsPlaying} setStatus={setStatus} setProgressMs={setProgressMs} setDevice={setDevice} />
+      </div> */}
+      </div>
+    </Router>
 
-          {privateRoutes.map((route, index) => {
-            const Element = route.element;
-            return (
-              <Route
-                key={index}
-                path={route.path}
-                element={<Element />}
-              />
-            );
-          })}
 
-          {adminRoutes.map((route, index) => {
-            const Element = route.element;
-            return (
-              <Route
-                key={index}
-                path={route.path}
-                element={<Element />}
-              />
-            );
-          })}
-        </Routes>
-      </Router>
   );
 }
 
---
+++ src/Components/SideBar/SideBarForAdmin.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { RxDashboard } from "react-icons/rx";
+import logo from '../../img/logo.jpg'
+import { FaRegUser } from "react-icons/fa";
+import { TbReportAnalytics } from "react-icons/tb";
+import { NavLink, useNavigate } from 'react-router-dom';
+
+function SideBarForAdmin() {
+
+    const navigate = useNavigate()
+    const changeRoute = () =>{ 
+        let path = `/`; 
+        navigate(path);
+      }
+
+  return (
+    <div className='md:flex flex-col gap-5 h-screen py-6 px-4 bg-[#121212] overflow-y-scroll w-[240px] transition-all overflow-hidden'>
+            <img src={logo} onClick={changeRoute} className={`transition-all cursor-pointer`}></img>
+
+            <NavLink to={`/`} className='flex flex-row gap-3'>
+                <RxDashboard className='text-[#EE5566] mt-2 text-lg'/>
+                <p className='text-white text-xl text-opacity-80'>Dashboard</p>
+            </NavLink>
+
+            <NavLink to={`/distributors`} className='flex flex-row gap-3'>
+                <FaRegUser className='text-[#EE5566] mt-2 text-lg'/>
+                <p className='text-white text-xl text-opacity-80'>Distributors</p>
+            </NavLink>
+
+            <NavLink to={`/users`} className='flex flex-row gap-3'>
+                <FaRegUser className='text-[#EE5566] mt-2 text-lg'/>
+                <p className='text-white text-xl text-opacity-80'>Users</p>
+            </NavLink>
+
+            <NavLink to={`/report`} className='flex flex-row gap-3'>
+                <TbReportAnalytics className='text-[#EE5566] mt-2 text-lg'/>
+                <p className='text-white text-xl text-opacity-80'>Report</p>
+            </NavLink>
+    </div>
+  )
+}
+
+export default SideBarForAdmin
